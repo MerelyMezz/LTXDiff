c588b9e0e196a6a4c8ee2ab76ae542cf234f68d5
mezzokoko <Mezzokoko@users.noreply.github.com>
2021-06-27 21:59:57 +0200

    Rootfile now needs to be specified for diff, which gets loaded fully before comparing with the mod

---
+++ LTXDiff/LTXDB.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using System.IO;
+
+
+namespace LTXDiff
+{
+    struct LTXIdentifier
+    {
+        string SectionName;
+        string VariableName;
+
+        public LTXIdentifier(string SectionName, string VariableName)
+        {
+            this.SectionName = SectionName;
+            this.VariableName = VariableName;
+        }
+    }
+
+    struct LTXDiffResult
+    {
+        public bool bWasSectionFound;
+        public bool bWasMatchFound;
+        public bool bWasDifferenceFound;
+    }
+
+    class LTXDB
+    {
+        Dictionary<string, string> SectionParents = new Dictionary<string, string>();
+        Dictionary<LTXIdentifier, string> VariableValues = new Dictionary<LTXIdentifier, string>();
+
+        public LTXDB(string RootFilePath)
+        {
+            if (!File.Exists(RootFilePath))
+            {
+                throw new Exception();
+            }
+
+            IEnumerable<LTXData> FileData = LTXDataFromFile(RootFilePath, false);
+
+            foreach (LTXData Data in FileData)
+            {
+                //Record Section Parent
+                string CurrentSectionParent;
+                bool bHasSectionParent = SectionParents.TryGetValue(Data.Section, out CurrentSectionParent);
+
+                if (!bHasSectionParent)
+                {
+                    SectionParents.Add(Data.Section, Data.SectionParent);
+                }
+                else if (CurrentSectionParent != Data.SectionParent)
+                {
+                    throw new Exception();
+                }
+
+                //Record Value
+                LTXIdentifier Identifier = new LTXIdentifier(Data.Section, Data.Key);
+
+                VariableValues[Identifier] = Data.Value;
+            }
+        }
+
+        public LTXDiffResult GetDiff(LTXData InputData)
+        {
+            LTXIdentifier Identifier = new LTXIdentifier(InputData.Section, InputData.Key);
+            LTXDiffResult Result;
+            Result.bWasSectionFound = SectionParents.ContainsKey(InputData.Section);
+            Result.bWasMatchFound = VariableValues.ContainsKey(Identifier);
+
+            if (Result.bWasMatchFound)
+            {
+                string Value;
+                VariableValues.TryGetValue(Identifier, out Value);
+
+                Result.bWasDifferenceFound = Value != InputData.Value;
+            }
+            else
+            {
+                Result.bWasDifferenceFound = false;
+            }
+
+            return Result;
+        }
+
+        public static IEnumerable<LTXData> LTXDataFromFile(string Filename, bool bIgnoreIncludes = true)
+        {
+            if (Path.GetExtension(Filename) != ".ltx" || !File.Exists(Filename))
+            {
+                yield break;
+            }
+
+            StreamReader SR = new StreamReader(File.OpenRead(Filename));
+
+            string CurrentSectionName = "";
+            string CurrentSectionParent = "";
+
+            while (!SR.EndOfStream)
+            {
+                string CurrentLine = SR.ReadLine();
+
+                //Clear Comments
+                int SemicolonPos = CurrentLine.IndexOf(';');
+                int SlashPos = CurrentLine.IndexOf('/');
+
+                if (SlashPos >= 0 && CurrentLine[SlashPos + 1] != '/')
+                {
+                    SlashPos = -1;
+                }
+
+                if (SlashPos * SemicolonPos <= 0)
+                {
+                    SlashPos = SlashPos >= 0 ? SlashPos : SemicolonPos + 1;
+                    SemicolonPos = SemicolonPos >= 0 ? SemicolonPos : SlashPos + 1;
+                }
+
+                int CommentPos = Math.Min(SlashPos, SemicolonPos);
+
+                if (CommentPos >= 0)
+                {
+                    CurrentLine = CurrentLine.Substring(0, CommentPos);
+                }
+
+                CurrentLine = CurrentLine.Trim();
+
+                //Empty Line
+                if (CurrentLine.Length == 0)
+                {
+                    continue;
+                }
+
+                //Including other LTX file
+                if (CurrentLine.Contains("#include"))
+                {
+                    if (bIgnoreIncludes)
+                    {
+                        continue;
+                    }
+
+                    Regex IncludeRegex = new Regex("(?<=#include\\s+\").+(?=\")");    //(?<=#include\s+").+(?=")
+                    Match IncludeMatch = IncludeRegex.Match(CurrentLine);
+
+                    if (!IncludeMatch.Success)
+                    {
+                        throw new Exception();
+                    }
+
+                    string IncludeFileName = Path.GetFullPath(IncludeMatch.Value, Path.GetDirectoryName(Filename));
+
+                    foreach (LTXData IncludeData in LTXDataFromFile(IncludeFileName, false))
+                    {
+                        yield return IncludeData;
+                    }
+                }
+
+                //Defining new section
+                if (CurrentLine[0] == '[' && CurrentLine.Contains(']'))
+                {
+                    CurrentSectionName = CurrentLine.Substring(1, CurrentLine.LastIndexOf(']') - 1).Trim();
+
+                    int ParentPos = CurrentLine.IndexOf(':');
+
+                    CurrentSectionParent = (ParentPos >= 0 && ParentPos <= CurrentLine.Length - 1) ? CurrentLine.Substring(ParentPos + 1, CurrentLine.Length - ParentPos - 1) : "";
+
+
+                    continue;
+                }
+
+                //Key Value Pair
+                string[] KeyValuePair = CurrentLine.Split("=");
+
+                if (KeyValuePair.Length == 2)
+                {
+                    string Key = KeyValuePair[0].Trim();
+                    string Value = KeyValuePair[1].Trim();
+
+                    yield return new LTXData(CurrentSectionName, CurrentSectionParent, Key, Value);
+                }
+            }
+        }
+    }
+}
--- LTXDiff/Program.cs
+++ LTXDiff/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.IO;
 
 using System.Collections.Generic;
+using System.IO;
 
 namespace LTXDiff
 {
-    struct SectionKeyValueTriple
+    struct LTXData
     {
         public string Section;
         public string SectionParent;
         public string Key;
         public string Value;
 
-        public SectionKeyValueTriple(string Section, string SectionParent, string Key, string Value)
+        public LTXData(string Section, string SectionParent, string Key, string Value)
         {
             this.Section = Section;
             this.SectionParent = SectionParent;
@@ -37,100 +37,27 @@ namespace LTXDiff
             System.Console.WriteLine(Input);
         }
 
-        static public IEnumerable<SectionKeyValueTriple> SectionKeyValueTripleFromFile(string Filename)
-        {
-            if (Path.GetExtension(Filename) != ".ltx" || !File.Exists(Filename))
-            {
-                yield break;
-            }
-
-            StreamReader SR = new StreamReader(File.OpenRead(Filename));
-
-            string CurrentSectionName = "";
-            string CurrentSectionParent = "";
-
-            while (!SR.EndOfStream)
-            {
-                string CurrentLine = SR.ReadLine();
-
-                //Clear Comments
-                int SemicolonPos = CurrentLine.IndexOf(';');
-                int SlashPos = CurrentLine.IndexOf('/');
-
-                if (SlashPos >= 0 && CurrentLine[SlashPos + 1] != '/')
-                {
-                    SlashPos = -1;
-                }
-
-                if (SlashPos * SemicolonPos <= 0)
-                {
-                    SlashPos = SlashPos >= 0 ? SlashPos : SemicolonPos + 1;
-                    SemicolonPos = SemicolonPos >= 0 ? SemicolonPos : SlashPos + 1;
-                }
-
-                int CommentPos = Math.Min(SlashPos, SemicolonPos);
-
-                if (CommentPos >= 0)
-                {
-                    CurrentLine = CurrentLine.Substring(0, CommentPos);
-                }
-
-                CurrentLine = CurrentLine.Trim();
-
-                //Empty Line
-                if (CurrentLine.Length == 0)
-                {
-                    continue;
-                }
-
-                //Including other LTX file, we don't care about this
-                if (CurrentLine.Contains("#include"))
-                {
-                    continue;
-                }
-
-                //Defining new section
-                if (CurrentLine[0] == '[' && CurrentLine.Contains(']'))
-                {
-                    CurrentSectionName = CurrentLine.Substring(1, CurrentLine.LastIndexOf(']') - 1).Trim();
-
-                    int ParentPos = CurrentLine.IndexOf(':');
-
-                    CurrentSectionParent = (ParentPos >= 0 && ParentPos <= CurrentLine.Length - 1) ? CurrentLine.Substring(ParentPos + 1, CurrentLine.Length - ParentPos - 1) : "";
-
-
-                    continue;
-                }
-
-                //Key Value Pair
-                string[] KeyValuePair = CurrentLine.Split("=");
-
-                if (KeyValuePair.Length == 2)
-                {
-                    string Key = KeyValuePair[0].Trim();
-                    string Value = KeyValuePair[1].Trim();
-
-                    yield return new SectionKeyValueTriple(CurrentSectionName, CurrentSectionParent, Key, Value);
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 3)
             {
-                System.Console.Error.WriteLine("LTXDiff [BaseDir] [ModDir]");
+                System.Console.Error.WriteLine("LTXDiff [BaseDir] [ModDir] [RootFile]");
                 return;
             }
 
             string BaseDir = FullPath(args[0]);
             string ModDir = FullPath(args[1]);
+            string RootFileName = Path.GetFullPath(args[2], BaseDir);
 
+            //Build LTX database
+            LTXDB BaseDataBase = new LTXDB(RootFileName);
+
+            //Compare and extract mod changes
             string[] AllModFileNames = System.IO.Directory.GetFiles(ModDir, "*", SearchOption.AllDirectories);
 
             foreach (string Filename in AllModFileNames)
             {
-                IEnumerable<SectionKeyValueTriple> ModFileData = SectionKeyValueTripleFromFile(Filename);
+                IEnumerable<LTXData> ModFileData = LTXDB.LTXDataFromFile(Filename);
 
                 string RelativePath = Path.GetRelativePath(ModDir, Filename);
                 string BaseDirFilename = Path.GetFullPath(RelativePath, BaseDir);
@@ -139,7 +66,7 @@ namespace LTXDiff
                 string CurrentSectionName = "";
                 string CurrentSectionParent = "";
 
-                foreach (SectionKeyValueTriple CurrentModData in ModFileData)
+                foreach (LTXData CurrentModData in ModFileData)
                 {
                     //Update section if need be
                     if (CurrentSectionName != CurrentModData.Section)
@@ -154,38 +81,14 @@ namespace LTXDiff
                         CurrentSectionParent = CurrentModData.SectionParent;
                     }
 
-                    //Compare Key Value pair with basefile
-                    IEnumerable<SectionKeyValueTriple> BaseFileData = SectionKeyValueTripleFromFile(BaseDirFilename);
+                    LTXDiffResult Result = BaseDataBase.GetDiff(CurrentModData);
 
-                    bool bWasSectionFound = false;
-                    bool bWasMatchFound = false;
-                    bool bWasDifferenceFound = false;
-
-                    foreach (SectionKeyValueTriple CurrentBaseData in BaseFileData)
-                    {
-                        if (CurrentBaseData.Section == CurrentModData.Section)
-                        {
-                            bWasSectionFound = true;
-
-                            if (CurrentBaseData.Key == CurrentModData.Key)
-                            {
-                                bWasMatchFound = true;
-
-                                if (CurrentBaseData.Value != CurrentModData.Value)
-                                {
-                                    bWasDifferenceFound = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    if (!bWasMatchFound || bWasDifferenceFound)
+                    if (!Result.bWasMatchFound || Result.bWasDifferenceFound)
                     {
                         if (!bIsCurrentSectionListed)
                         {
                             bIsCurrentSectionListed = true;
-                            Print((bWasSectionFound ? "![" : "[") + CurrentSectionName + "]" + (!bWasSectionFound && CurrentSectionParent.Length > 0 ? ":" + CurrentSectionParent : ""));
+                            Print((Result.bWasSectionFound ? "![" : "[") + CurrentSectionName + "]" + (!Result.bWasSectionFound && CurrentSectionParent.Length > 0 ? ":" + CurrentSectionParent : ""));
                         }
 
                         Print(CurrentModData.Key + " = " + CurrentModData.Value);
